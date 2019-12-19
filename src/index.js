5349f866a4cb134cc3418143267f1b381ff092d8
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-12-19 09:04:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
-// It might be a good idea to add event listener to make sure this file 
-// only runs after the DOM has finshed loading. 
+document.addEventListener('DOMContentLoaded',() => {
+  getQuotes()
+  
+})
+
+function getQuotes(){
+
+    fetch('http://localhost:3000/quotes?_embed=likes')
+    .then(response => response.json())
+    .then(quotes => quotes.forEach(quote => {displayQuotes(quote)}))
+}
+
+function displayQuotes(quote){
+
+    let quoteList        = document.querySelector('#quote-list')
+    let quoteLi          = document.createElement('li')
+    let blockquote       = document.createElement('blockquote')
+    let quoteText        = document.createElement('p')
+    let authorFooter     = document.createElement('footer')
+    let likeButton       = document.createElement('button')
+    let deleteButton     = document.createElement('button')
+
+    blockquote.classList.add('blockquote')
+    quoteText.classList.add('mb-0')
+    authorFooter.classList.add('blockquote-footer')
+    likeButton.classList.add('btn-success')
+    deleteButton.classList.add('btn-danger')
+
+    quoteText.innerText    = quote.quote
+    authorFooter.innerText = quote.author
+    likeButton.innerText   = 'Like'
+    deleteButton.innerText = 'delete'
+
+    quoteList.appendChild(quoteLi)
+    quoteLi.appendChild(blockquote)
+    blockquote.append(quoteText, authorFooter, likeButton, deleteButton)
+
+}
